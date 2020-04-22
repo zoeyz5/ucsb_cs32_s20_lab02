ee13fc26c4131a2784efa5abb3df7066ea4bf732
zoeyz5 <xiaozhou@umail.ucsb.edu>
2020-04-22 14:57:29 +0800

    Update studentRoll.cpp

--- studentRoll.cpp
+++ studentRoll.cpp
@@ -6,33 +6,33 @@ StudentRoll::StudentRoll() {
 }
 
 void StudentRoll::insertAtTail(const Student &s) {
-	Node* new_node = new Node;
-	Student* a = new Student(s);
-	new_node->s = a;
-	new_node->next = NULL;
+    Node* new_node = new Node;
+    Student* a = new Student(s);
+    new_node->s = a;
+    new_node->next = NULL;
 
-	if(head == NULL){
-		head = new_node;
-		tail = new_node;
-	}
-	else{
-	tail->next = new_node;
-	tail = new_node;
-	}
+    if(head == NULL){
+        head = new_node;
+        tail = new_node;
+    }
+    else{
+    tail->next = new_node;
+    tail = new_node;
+    }
 }
 
 std::string StudentRoll::toString() const {
-	std::string output = "[";
-	Node *temp = head;
-	while(temp){
-		output= output+ temp -> s -> toString();
-		if(temp != tail){
-			output=output+",";
-		}
-		temp = temp ->next;
-	}
-	output = output +"]";
-	return output;
+    std::string output = "[";
+    Node *temp = head;
+    while(temp){
+        output= output+ temp -> s -> toString();
+        if(temp != tail){
+            output=output+",";
+        }
+        temp = temp ->next;
+    }
+    output = output +"]";
+    return output;
 
 }
 
@@ -41,38 +41,49 @@ StudentRoll::StudentRoll(const StudentRoll &orig) {
   head = tail = NULL;
   Node *temp = orig.head;
   while(temp){
-	  insertAtTail(*(temp->s));
-	  temp = temp -> next;
+      insertAtTail(*(temp->s));
+      temp = temp -> next;
   }
 }
 
 StudentRoll::~StudentRoll() {
   // STUB
-  Node* temp = head;
-  while(temp != NULL){
-	Node* temp2 = temp;
-	temp = temp->next;
-	delete temp2->s;
-	delete temp2;
-  }  
+  Node *current = head;
+  while(current != NULL){
+      Node *next = current->next;
+      delete current->s;
+      delete current;
+      current = next;
+  }
+  head = NULL;
 }
 
 StudentRoll & StudentRoll::operator =(const StudentRoll &right ) {
   // The next two lines are standard, and you should keep them.
-  // They avoid problems with self-assignment where you might free up 
+  // They avoid problems with self-assignment where you might free up
   // memory before you copy from it.  (e.g. x = x)
 
-  if (&right == this) 
+  if (&right == this)
     return (*this);
-  // TODO... Here is where there is code missing that you need to 
+  // TODO... Here is where there is code missing that you need to
   // fill in...
-	Node *temp = right.head;
-	while(temp){
-		insertAtTail(*(temp->s));
-		temp = temp ->next;
-	}
+    Node *current = head;
+    while(current != NULL){
+        Node *next = current->next;
+        delete current->s;
+        delete current;
+        current = next;
+    }
+    head = NULL;
+    
+    Node *temp = right.head;
+    while(temp){
+        insertAtTail(*(temp->s));
+        temp = temp ->next;
+    }
   // KEEP THE CODE BELOW THIS LINE
   // Overloaded = should end with this line, despite what the textbook says.
-  return (*this); 
+  return (*this);
   
 }
+
